23b94680435af62228552579f15d185b79274881
Siddharth Sangwan <s.sngwn.me@gmail.com>
2022-05-28 23:43:32 +0530

    Complete

--- Kali_Linux_Setup.sh
+++ Kali_Linux_Setup.sh
@@ -3,13 +3,13 @@ if [[ $EUID -eq 0 ]]; then
     # Download Burp Suite Profesional Latet Version
     echo "Downloading Burp Suite Professional ...."
     Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar"
-    wget "$Link" -O Burp_Suite_Pro.jar
+    wget "$Link" -O Burp_Suite_Pro.jar --quiet --show-progress
     sleep 2
 
     # execute Keygenerator
     echo "Starting Keygenerator"
     (java -jar keygen.jar) &
-    sleep 5s
+    sleep 3s
     
     # Execute Burp Suite Professional with Keyloader
     echo "Executing Burp Suite Professional with Keyloader"
