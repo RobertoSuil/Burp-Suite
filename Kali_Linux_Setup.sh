0c5f68ccc18bb52eda93b52e44055136b466e5c2
Siddharth Sangwan <s.sngwn.me@gmail.com>
2022-05-28 23:34:11 +0530

    Update

--- Kali_Linux_Setup.sh
+++ Kali_Linux_Setup.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 if [[ $EUID -eq 0 ]]; then
-    # Check for Latest Version
-    echo "Checking Latest Burp Professional Version"
-    wget https://portswigger.net/burp/releases/professional/latest -O Page -b
-    latest_burp=$(less Page|grep '<meta property="og:url" content="' |cut -d '"' -f 4)
-    rm Page
-    sleep 1
-    clear
-    wget $latest_burp -O Page -b
-    version=$(less Page|grep 'version=' |cut -d '=' -f 5|cut -d '&' -f 1|head -1)
-    rm Page
-    sleep 50
-    clear
-    
     # Download Burp Suite Profesional Latet Version
     echo "Downloading Burp Suite Professional ...."
-    Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=$version&type=jar"
+    Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar"
     wget "$Link" -O Burp_Suite_Pro.jar
-    clear
+    sleep 2
 
     # execute Keygenerator
     echo "Starting Keygenerator"
