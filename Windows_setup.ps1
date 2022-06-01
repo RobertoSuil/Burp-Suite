b759280788be07082c6cedee55e661e412fffade
Siddharth Sangwan <36560000+SNGWN@users.noreply.github.com>
2022-06-02 00:02:07 +0530

    Final Commit

--- Windows_setup.ps1
+++ Windows_setup.ps1
@@ -10,7 +10,7 @@ echo "
 "
 
 # Set Wget Progress to Silent, Becuase it slows down Downloading by +50x
-echo "Setting Wget Progress to Silent, Becuase it slows down Downloading by +50x"
+echo "Setting Wget Progress to Silent, Becuase it slows down Downloading by +50x`n"
 $ProgressPreference = 'SilentlyContinue'
 
 # Check JDK-18 Availability or Download JDK-18
@@ -45,21 +45,27 @@ if (Test-Path Burp-Suite-Pro.jar){
     if (((Get-Item Burp-Suite-Pro.jar).length/1MB) -lt 500 ){
         echo "`n`t`tFiles Seems to be corrupted `n`t`tDownloading Latest Burp Suite Professional ...."
         wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -O "Burp-Suite-Pro.jar"
-        echo "Burp Suite Professional is Downloaded.`n"
+        echo "`nBurp Suite Professional is Downloaded.`n"
     }else {echo "File Looks fine. Lets proceed for Execution"}
 }else {
     echo "`n`t`tDownloading Latest Burp Suite Professional ...."
     wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -O Burp-Suite-Pro.jar
-    echo "Burp Suite Professional is Downloaded.`n"
+    echo "`nBurp Suite Professional is Downloaded.`n"
 }
 
+
+# Lets Activate Burp Suite Professional with keygenerator and Keyloader
+echo "`n`nStarting Keygenerator ...."
+start-process java.exe -argumentlist "-jar keygen.jar"
+echo "`n`nStarting Burp Suite Professional"
+java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -javaagent:"loader.jar" -noverify -jar "Burp-Suite-Pro.jar"
 
 
 # Creating Burp.bat file with command for execution
 if (Test-Path burp.bat) {rm burp.bat} 
 $path = "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -javaagent:`"$pwd\loader.jar`" -noverify -jar `"$pwd\Burp-Suite-Pro.jar`""
 $path | add-content -path Burp.bat
-echo "Burp.bat file is created`n"
+echo "`nBurp.bat file is created"
 
 
 # Creating Burp-Suite-Pro.vbs File for background execution
@@ -68,10 +74,8 @@ if (Test-Path Burp-Suite-Pro.vbs) {
 echo "Set WshShell = CreateObject(`"WScript.Shell`")" > Burp-Suite-Pro.vbs
 add-content Burp-Suite-Pro.vbs "WshShell.Run chr(34) & `"$pwd\Burp.bat`" & Chr(34), 0"
 add-content Burp-Suite-Pro.vbs "Set WshShell = Nothing"
-echo "Burp-Suite-Pro.vbs file is created.`n"
+echo "`nBurp-Suite-Pro.vbs file is created."
 
-# Lets Activate Burp Suite Professional with keygenerator and Keyloader
-echo "`n`nStarting Keygenerator ...."
-start-process java.exe -argumentlist "-jar keygen.jar"
-echo "`n`nStarting Burp Suite Professional"
-java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -javaagent:"loader.jar" -noverify -jar "Burp-Suite-Pro.jar"
+# Remove Additional files
+rm Kali_Linux_Setup.sh
+del -Recurse -Force .\.github\
